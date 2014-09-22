1af27b95756977de2672ad3034abe7a27ac2c5ae
Sergio Sebastian Arriagada <sergio@nibyru.com>
2014-09-21 21:53:07 -0300

    fix capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :keep_releases, 5
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
 
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
