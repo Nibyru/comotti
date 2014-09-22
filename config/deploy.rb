7d9a45a0bad00f61c85ad9945ce23b459fe0e451
Sergio Sebastian Arriagada <sergio@nibyru.com>
2014-09-21 22:00:32 -0300

    fix assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,8 +19,50 @@ set :keep_releases, 5
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+# set the locations that we will look for changed assets to determine whether to precompile
+set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock config/routes.rb)
 
+# clear the previous precompile task
+Rake::Task["deploy:assets:precompile"].clear_actions
+class PrecompileRequired < StandardError; end
+
+namespace :deploy do
+  namespace :assets do
+    desc "Precompile assets"
+    task :precompile do
+      on roles(fetch(:assets_roles)) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            begin
+              # find the most recent release
+              latest_release = capture(:ls, '-xr', releases_path).split[1]
+
+              # precompile if this is the first deploy
+              raise PrecompileRequired unless latest_release
+
+              latest_release_path = releases_path.join(latest_release)
+
+              # precompile if the previous deploy failed to finish precompiling
+              execute(:ls, latest_release_path.join('assets_manifest_backup')) rescue raise(PrecompileRequired)
+
+              fetch(:assets_dependencies).each do |dep|
+                # execute raises if there is a diff
+                execute(:diff, '-Naur', release_path.join(dep), latest_release_path.join(dep)) rescue raise(PrecompileRequired)
+              end
+
+              info("Skipping asset precompile, no asset diff found")
+
+              # copy over all of the assets from the last release
+              execute(:cp, '-r', latest_release_path.join('public', fetch(:assets_prefix)), release_path.join('public', fetch(:assets_prefix)))
+            rescue PrecompileRequired
+              execute(:rake, "assets:precompile") 
+            end
+          end
+        end
+      end
+    end
+  end
+end
 # what specs should be run before deployment is allowed to
 # continue, see lib/capistrano/tasks/run_tests.cap
 # set :tests, []
