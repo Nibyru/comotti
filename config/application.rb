24504fdf45aa15c2743a18e9afeaf33f7fabb658
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-10 00:40:12 -0300

    add dropbox

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Comotti
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    CarrierWave.configure do |config|
+      config.dropbox_app_key = "q1ettyi0ncao687"
+      config.dropbox_app_secret = "xa6j56v6q7k9g0s"
+      config.dropbox_access_token = "pnf5h8fdy11uyna"
+      config.dropbox_access_token_secret = "7mirojnoqmagn1a"
+      config.dropbox_user_id = "54889672"
+      config.dropbox_access_type = "app_folder"
+    end
   end
 end
