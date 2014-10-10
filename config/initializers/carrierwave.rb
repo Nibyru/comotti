24b51f4427b57c7f4bab8477275e1e979401638e
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-10 00:48:45 -0300

    add dropbox config

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.dropbox_app_key = "q1ettyi0ncao687"
+  config.dropbox_app_secret = "xa6j56v6q7k9g0s"
+  config.dropbox_access_token = "pnf5h8fdy11uyna"
+  config.dropbox_access_token_secret = "7mirojnoqmagn1a"
+  config.dropbox_user_id = "54889672"
+  config.dropbox_access_type = "app_folder"
+end
