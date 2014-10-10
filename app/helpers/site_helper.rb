7589c1786faba44535adc9145a8c98ec69ebb6b1
Sergio Sebastian Arriagada <sergio@nibyru.com>
2014-10-09 21:05:10 -0300

    add category examples

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,9 +1,11 @@
 module SiteHelper
   def photo_example(category)
     offset = rand(category.products.count - 1)
-    if category.products.any?
+    if category.products.any? && !category.products.offset(offset).first.photo.url.nil?
+      puts ">>> 1"
       category.products.offset(offset).first.photo.url
     else
+      puts ">>> 2"
       image_url 'category-example.jpg'
     end
   end
