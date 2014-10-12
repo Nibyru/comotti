5d42e3dc5685c70d017cfeda47d3018e38875039
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-12 19:08:11 -0300

    fix uploader

--- app/uploaders/banner_uploader.rb
+++ app/uploaders/banner_uploader.rb
@@ -11,12 +11,10 @@ class BannerUploader < CarrierWave::Uploader::Base
   storage :file
   # storage :fog
 
-  permissions 0777
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
