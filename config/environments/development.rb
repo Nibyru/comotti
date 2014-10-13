6b50c3ced0e40a8c3834dc43ec8045fa88b424c2
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-13 04:11:56 -0300

    remove devise sign-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,15 +26,4 @@ Comotti::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
-  # local config
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'sarriagada87@gmail.com',
-    password:             'yosoymuyfeliz',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
 end
