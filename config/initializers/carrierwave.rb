30f7c5da7063bf5973895139c1e80ee00c317e07
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-13 11:54:33 -0300

    update gitignore

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-        :provider                         => 'Google',
-        :google_storage_access_key_id     => 'GOOGDVE7HFBDYEEB3F6O',
-        :google_storage_secret_access_key => '6fHVXL0LqsTCcbNmyd7bhEMk1RT7pASrhg5VUu0e'
-  }
-  config.fog_directory = 'comotti'
-end
