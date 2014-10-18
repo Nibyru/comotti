8df7b4c596e59a3e95ffc6b00059a325c2ecd8b0
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2014-10-18 13:42:23 -0300

    update config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
         :provider                         => 'Google',
-        :google_storage_access_key_id     => 'GOOGDVE7HFBDYEEB3F6O',
-        :google_storage_secret_access_key => '6fHVXL0LqsTCcbNmyd7bhEMk1RT7pASrhg5VUu0e'
+        :google_storage_access_key_id     => 'GOOGGRGVM6SNO7MWRUC7',
+        :google_storage_secret_access_key => 'znqSln89ld1xQaTtDiAKCkQGbn6/isptUkwMoZmH'
   }
   config.fog_directory = 'comotti'
 end
