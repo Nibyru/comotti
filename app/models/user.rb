b278750b3b8e9c0e5ad51552ee1d6524e0552aee
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2015-01-30 01:03:02 -0300

    add new relic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessible :email, :password
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 end
